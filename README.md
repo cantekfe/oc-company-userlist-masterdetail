4e362c1361837557ffcee560c8e034e094554a34
cantekfe <cantek.frontend@gmail.com>
2019-05-10 16:41:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# oc-company-userlist-masterdetail
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/oc-company-userlist-masterdetail)
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+
+import { CompanyListComponent } from './company-list/company-list.component';
+import { CompanyDetailsComponent } from './company-details/company-details.component';
+import {MatTableModule ,MatIconModule,MatCardModule,MatButtonModule } from '@angular/material';
+import { CompanyService } from './services/company.service';
+import { SettingsService } from './services/settings.service';
+
+import { CompanyContainerComponent } from './company-container/company-container.component';
+import { CompanySettingsComponent } from './company-settings/company-settings.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule
+  ],
+  declarations: [
+    AppComponent,
+    CompanyListComponent,
+    CompanyDetailsComponent,
+    CompanyContainerComponent,
+    CompanySettingsComponent],
+  bootstrap: [AppComponent],
+  providers: [CompanyService,SettingsService]
+})
+export class AppModule { }
