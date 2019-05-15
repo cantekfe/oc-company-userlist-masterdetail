d63cbb94c2aa36c4182aad0418c0a34812a6d273
melike <melike.goksoy@cantekgroup.com>
2019-05-15 17:12:17 +0300

    versiyonlandi

--- projects/c-company-user-list/src/lib/company-container/company-container.component.ts
+++ projects/c-company-user-list/src/lib/company-container/company-container.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ToolboxComponent } from '../toolbox-decorator';
 
+ @ToolboxComponent({
+   desc: 'Barchart Component',
+   icon: 'fa fa-chart-bar',
+   componentName : 'CompanyContainerComponent'
+ })
 @Component({
   selector: 'app-company-container',
   templateUrl: './company-container.component.html',
